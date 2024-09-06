af95f8701f38f81d648e72cb027b619c3cce9717
HIJOdelIDANII <ahmed.idani@insat.ucar.tn>
2024-09-06 02:51:50 +0100

    probleme navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,20 +3,28 @@ import {Link} from "react-router-dom";
 import {useState} from "react";
 import logo from "../../images/image.png"
 export const Navbar = ()=>{
+    /* toggle1 for the pc version : clicking the three bars results in a div extra-info-1 (à propos contact) */
+    /* toggle is for small tablets and smartphone to toggle the sidenavbar*/
     const [toggle1,setToggle1] = useState(false);
+    const [toggle2,setToggle2] = useState(true);
+
+    const handleToggle2 = (e)=>{
+        setToggle2(!toggle2);
+    }
     const handleToggle1 = (e)=>{
         e.currentTarget.blur();  // Remove focus after click
         setToggle1(!toggle1);
     }
     return(
     <section className="navbar">
-        <div className="navbar-container">
+        <div className = { `navbar-container ${toggle2? "": "toggle-navbar"} `}>
             
             <div className="links-container left">
                     
                 <Link className="roboto-bold" to="/reparation-montres">Réparation</Link>
                 <Link className="roboto-bold" to="/devis-reparation">Devis de réparation</Link>
-                    
+                <Link  className=" remove-link roboto-bold" to="about">à propos</Link>
+                
                
             </div>
             
@@ -31,20 +39,26 @@ export const Navbar = ()=>{
                 </Link>
                 <p className="slogan">swiss furniture</p>
             </div>
-           
             <div className="links-container right">
                 <Link className="roboto-bold" to="/vente-montres">Vente de montres</Link>
                 <Link className="roboto-bold" to="/temoignages">Témoignages</Link>
-                <div className="toggle_button">
+                <Link  className=" remove-link roboto-bold" to="contact">contact</Link>    
+                
+                <div className="remove-bars-1 toggle_button">
                     <i onClick={(e)=>{handleToggle1(e)}} className="fa-solid fa-bars bars"></i>
                 </div>  
+                <div className="remove-bars-2 toggle_button">
+                    <i onClick={(e)=>{handleToggle2(e)}} className="fa-solid fa-bars bars"></i>
+                </div> 
 
             </div> 
                   
-            
+        </div>
+        <div className ={`bars-phone ${toggle2? "bars-removal": "bars"} `} >
+            <i onClick={(e)=>{handleToggle2(e)}} className="fa-solid fa-bars bars"></i>
         </div>
         
-        <div className ={`toggle-extra-information-1 ${toggle1 ? "": "no-toggle-info-1"}`}> 
+        <div className ={`remove-bars-1 toggle-extra-information-1 ${toggle1 ? "": "no-toggle-info-1"}`}> 
             <Link  className="roboto-bold" to="about">à propos</Link>
             <Link  className="roboto-bold" to="contact">contact</Link>
         </div>
