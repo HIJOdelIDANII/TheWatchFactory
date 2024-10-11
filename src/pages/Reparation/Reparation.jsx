37d10a7b4e2769de45797735f386b362f5b0fa99
HIJOdelIDANII <ahmed.idani@insat.ucar.tn>
2024-10-11 21:31:48 +0100

    update reparation page

--- src/pages/Reparation/Reparation.jsx
+++ src/pages/Reparation/Reparation.jsx
@@ -1,9 +1,28 @@
 import "./Reparation.css"
-
+import { WatchRepairTab } from "../../components/WatchRepairTab/WatchRepairTab.jsx"
+import image1 from "../../images/shutterstock.jpg"
 export const Reparation = ()=>{
     return(
         <div className="reparation-container">
-            <h1>Reparation</h1>
+            <div className="menu-image-container">
+                <img className="menu-image" src={image1} alt="watch-image"/>
+            </div>
+            <WatchRepairTab></WatchRepairTab>
+            <div className="reparation-description-container">
+                <div className="description-1 desc">
+                    <div className="text-1">
+
+                    </div>
+                    
+                </div>
+                <div className="description-2 desc">
+                    <div className="text-2">
+
+                    </div>
+                </div>
+
+            </div>
+
         </div>
     )
 }
