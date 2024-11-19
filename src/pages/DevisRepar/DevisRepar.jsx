e3b076f19da2b9c1d0c79cd7dc554ced7447c696
HIJOdelIDANII <ahmed.idani@insat.ucar.tn>
2024-11-19 17:34:04 +0100

    need to add image for rating

--- src/pages/DevisRepar/DevisRepar.jsx
+++ src/pages/DevisRepar/DevisRepar.jsx
@@ -1,6 +1,11 @@
 import "./DevisRepar.css"
 import image1 from "../../images/watch-devis.png"
 export const DevisRepar =  () => {
+    let ArrayRatings = [{name:"ahmed",rating:"5"},{name:"ahmed",rating:"5"},{name:"ahmed",rating:"5"},
+        {name:"ahmed",rating:"5"},{name:"ahmed",rating:"5"}
+
+    ];
+    
     return(
         <div className="watch-repair-container">
             <div className="watch-repair-main-container">
@@ -15,8 +20,30 @@ export const DevisRepar =  () => {
             <div className="watch-repair-image-container">
                 <img src={image1} className="watch-repair-image" alt="Mouvement de Montre" />
             </div>
-            
+            <div className="watch-repair-details">
+                <img className="image-repair-details" alt="image-repair-detail"/>
+                <p className="description-repair-details">
+                    The Watch Factory est reconnue pour son expertise exceptionnelle dans la réparation de montres.
+                     Grâce à des années d'expérience et à un savoir-faire unique, notre équipe restaure et entretient vos garde-temps avec précision et soin,
+                     garantissant leur durabilité et leur performance.
+                </p>
+            </div>
+            <div className="watch-repair-feedback">
+                {
+                    ArrayRatings.forEach((val)=> 
+                        {return(
+                        <div className="rating-container">
+                            <p className="name-rating"> {val.name}</p>
+                            <div className="rating-rating"> </div>
+                        </div>)}
+                    
+                        
+                    )
+                }
+            </div>
+
         </div>
+        
     
     )
 }
